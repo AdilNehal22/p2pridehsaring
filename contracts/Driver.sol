727349687b0db5e01773594f14a1c0faad17aadd
AdilNehal22 <adilnehal80@gmail.com>
2022-07-31 20:44:25 +0500

    created factory

--- contracts/Driver.sol
+++ contracts/Driver.sol
@@ -1,11 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+contract DriverFactory {
+
+  address[] public deployedDrivers;
+
+  function createProjectFund() public {
+    Driver createNewDriver =  new Driver(msg.sender);
+    deployedDrivers.push(address(createNewDriver));
+  }
+
+  function getAllRegisteredDrivers() public view returns(address[] memory) {
+    return deployedDrivers;
+  }
+
+}
+
 contract Driver {
 
   //struct for driver definition
   struct Infodriver{
-    address riderAddress;
+    address engagedRiderAddress;
     address driverAddress;
     bytes32 driverID;
     uint256 numberOfServices;
@@ -19,23 +34,20 @@ contract Driver {
   }
 
   event inService(address indexed _passenger, bytes32 indexed driverID, uint256 numOfTokensDecided);
-
   driverState constant driverDefaultState = driverState.RIDE_EMPTY;
   mapping(address => bytes32) public IdToDriver;
+
   //to access the struct data inside a constructor
   Infodriver private driver;
 
-  constructor(){
-    driver.driverAddress = msg.sender;
-    driver.driverID = keccak256(abi.encodePacked((block.timestamp)**3, msg.sender));
+  constructor(address _newDriver){
+    driver.driverAddress = _newDriver;
+    driver.driverID = keccak256(abi.encodePacked((block.timestamp)**3, _newDriver));
   }
 
-  // function getDriverID() public view returns (bytes32){
-  //   return 
-  // }
-  function driverInService(address _passenger) public {
+  function createDriverService(address _passenger) public {
     require(driverState.RIDE_EMPTY == driverDefaultState, "Driver must clear all the services before starting any new one");
-    
+
   }
 
 
