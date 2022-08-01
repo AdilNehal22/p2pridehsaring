6473e49689bc937b5b2f5ad6574bb864e6e845a4
AdilNehal22 <adilnehal80@gmail.com>
2022-08-01 10:12:08 +0500

    testing initial functionalities

--- test/Driver.ts
+++ test/Driver.ts
@@ -5,10 +5,15 @@ const {
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 const { ethers } = require('hardhat');
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { DriverFactory } from "../typechain-types";
+
+
+let Driverfactory: DriverFactory;
 
 describe("main testing of driver", function () {
-  describe("", function(){
+  describe("factory contract creates new driver", function(){
     
-  })
+  });
 
 });
