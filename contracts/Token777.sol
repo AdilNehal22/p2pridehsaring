ef1f283abf0537b20415cad834dcde02e612e663
AdilNehal22 <adilnehal80@gmail.com>
2022-08-12 01:14:37 +0500

    token development

--- contracts/Token777.sol
+++ contracts/Token777.sol
@@ -1,3 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+
+contract Atoken777 is ERC777 {
+  
+  constructor() public ERC777("TokenFare","P2PRS", new address[](0)){
+    // _mint(account, amount, userData, operatorData);
+  }
+} 
