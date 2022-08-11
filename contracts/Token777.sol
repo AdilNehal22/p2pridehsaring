b1d7742aa2ddccfd1b3a750818f26d5b755a748f
AdilNehal22 <adilnehal80@gmail.com>
2022-08-12 01:35:28 +0500

    token dev

--- contracts/Token777.sol
+++ contracts/Token777.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 
 contract Atoken777 is ERC777 {
-  
-  constructor() public ERC777("TokenFare","P2PRS", new address[](0)){
+
+  string public constant tokenName = "TokenFare";
+  string public constant tokenSymbol = "P2PRS";
+  uint256 public initialTokenSupply = 250000;
+
+  constructor() public ERC777(tokenName,tokenSymbol, new address[](0)){
     // _mint(account, amount, userData, operatorData);
   }
 } 
